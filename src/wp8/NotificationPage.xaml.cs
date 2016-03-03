cfe5d367b1049822b9b41ec465038f5f5a4f98ed
Gary Meehan <wingstand@googlemail.com>
2016-03-03 15:08:34 +0000

    First pass at push/local notifications for Windows Phone 8.

---
+++ src/wp8/NotificationPage.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Net;
+using System.Windows.Navigation;
+
+using Microsoft.Phone.Controls;
+
+namespace WPCordovaClassLib.Cordova.Commands
+{
+  public partial class NotificationPage : PhoneApplicationPage
+  {
+    public NotificationPage()
+    {
+      InitializeComponent();
+    }
+
+    protected override void OnNavigatedTo(NavigationEventArgs e)
+    {
+      base.OnNavigatedTo(e);
+
+      Notification.QueueNotificationEvent(e.Uri);
+
+      this.NavigationService.Navigate(new Uri("/MainPage.xaml", UriKind.Relative));
+    }
+
+    protected override void OnNavigatedFrom(NavigationEventArgs e)
+    {
+      base.OnNavigatedFrom(e);
+
+      // Make sure we can't navigate back here.
+      NavigationService.RemoveBackEntry();
+    }
+  }
+}
